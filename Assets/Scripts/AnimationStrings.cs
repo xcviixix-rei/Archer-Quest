483764160a191e05df43f20d8b7083ccd882cec1
xcviixix-rei <124432779+xcviixix-rei@users.noreply.github.com>
2025-05-21 23:29:23 +0700

    Create Player basic animations

---
+++ Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,7 @@
+using System;
+internal class AnimationStrings
+{
+    internal static string isMoving = "isMoving";
+    internal static string isRunning = "isRunning";
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,65 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 5f;
+    Animator animator;
+    public float walkSpeed = 3f;
+    public float runSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D rb;
-    private bool isMoving { get; set; }
+    private bool _isMoving = false;
+    private bool _isRunning = false;
+    private bool _isFacingRight = true;
+
+    public bool isMoving
+    {
+        get { return _isMoving; }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
+    }
+
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.isRunning, value);
+        }
+    }
+    
+    public bool isFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        }
+    }
+
+    public float currentSpeed
+    {
+        get
+        {
+            if (isMoving) return _isRunning ? runSpeed : walkSpeed;
+            else return 0f;
+        }
+    }
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,12 +76,37 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveInput.x * currentSpeed, rb.linearVelocity.y);
     }
 
     public void onMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
         isMoving = moveInput != Vector2.zero;
+        setFacingDirection(moveInput);
+    }
+
+    private void setFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !isFacingRight)
+        {
+            isFacingRight = true;
+        }
+        else if (moveInput.x < 0 && isFacingRight)
+        {
+            isFacingRight = false;
+        }
+    }
+
+    public void onRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isRunning = true;
+        }
+        else if (context.canceled)
+        {
+            isRunning = false;
+        }
     }
 }
